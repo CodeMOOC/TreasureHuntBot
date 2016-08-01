58ba440dae039a0bcbc9690dcb77fadf7151436e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-08-01 17:17:07 +0200

    Add conversation table to DB

--- sql/treasurehuntbot.sql
+++ sql/treasurehuntbot.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 01, 2016 at 03:21 PM
+-- Generation Time: Aug 01, 2016 at 05:11 PM
 -- Server version: 5.5.46-0+deb8u1
 -- PHP Version: 5.6.17-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `treasurehuntbot`
@@ -46,6 +40,17 @@ CREATE TABLE `assigned_riddles` (
   `group_id` int(11) NOT NULL,
   `timestamp` datetime NOT NULL,
   `last_answer` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conversation`
+--
+
+CREATE TABLE `conversation` (
+  `telegram_user_id` int(11) NOT NULL,
+  `state` tinyint(3) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -154,6 +159,12 @@ ALTER TABLE `assigned_riddles`
   ADD KEY `assriddles_group_constraint` (`group_id`);
 
 --
+-- Indexes for table `conversation`
+--
+ALTER TABLE `conversation`
+  ADD PRIMARY KEY (`telegram_user_id`);
+
+--
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
@@ -229,7 +240,3 @@ ALTER TABLE `status`
 --
 ALTER TABLE `tracks`
   ADD CONSTRAINT `track_location_constraint` FOREIGN KEY (`game_id`,`location_id`) REFERENCES `locations` (`game_id`, `id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
