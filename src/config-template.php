53577d3fdd0945213d01ab9ca7cdfa8e0b61f7bc
Brendan Paolini <brendan.paolini@gmail.com>
2016-08-19 16:00:07 +0200

    added proper config-template

---
+++ src/config-template.php
@@ -0,0 +1,18 @@
+<?php
+
+/*  Constants for telegram API */
+define('TELEGRAM_BOT_TOKEN', '');
+define('TELEGRAM_API_URI_BASE', 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/');
+define('TELEGRAM_API_URI_FILE', 'https://api.telegram.org/file/bot' . TELEGRAM_BOT_TOKEN . '/');
+define('TELEGRAM_API_URI_ME', TELEGRAM_API_URI_BASE . 'getMe');
+define('TELEGRAM_API_URI_MESSAGE', TELEGRAM_API_URI_BASE . 'sendMessage');
+define('TELEGRAM_API_URI_LOCATION', TELEGRAM_API_URI_BASE . 'sendLocation');
+define('TELEGRAM_API_URI_PHOTO', TELEGRAM_API_URI_BASE . 'sendPhoto');
+define('TELEGRAM_API_URI_UPDATES', TELEGRAM_API_URI_BASE . 'getUpdates');
+define ('TELEGRAM_API_URI_FILE_PATH', TELEGRAM_API_URI_BASE . 'getFile');
+
+/*  Constants for DB Access */
+define('DATABASE_HOST', '');
+define('DATABASE_NAME', '');
+define('DATABASE_USERNAME', '');
+define('DATABASE_PASSWORD', '');
