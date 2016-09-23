2ed965350204b279918d9e5346a02f70144d231c
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-09-23 16:49:24 +0200

    Minor logging improvements

--- src/pull.php
+++ src/pull.php
@@ -17,18 +17,18 @@ $last_update = @file_get_contents("pull-last-update.txt");
 // Note: we remember the last fetched ID and query for the next one, if available.
 //       The third parameter enabled long-polling. Switch to any number of seconds
 //       to enable (the request will hang until timeout or until a message is received).
-$content = telegram_get_updates(intval($last_update) + 1, 1, 60);
+$content = telegram_get_updates(intval($last_update) + 1, null, 60);
 if($content === false) {
-    Logger::fatal('Failed to fetch updates from API');
+    Logger::fatal('Failed to fetch updates from API', __FILE__, $context););
 }
 if(count($content) == 0) {
-    Logger::debug('No new messages');
+    Logger::debug('No new messages', __FILE__, $context););
     exit;
 }
 
 $first_update = $content[0];
 
-Logger::debug('New update: ' . print_r($first_update, true));
+Logger::debug('New update: ' . print_r($first_update, true), __FILE__, $context););
 
 // Updates have the following structure:
 // [
@@ -47,7 +47,7 @@ $message = $first_update['message'];
 file_put_contents("pull-last-update.txt", $update_id);
 
 if(!$message) {
-    Logger::fatal('Non-message update received');
+    Logger::fatal('Non-message update received', __FILE__, $context););
 }
 
 include 'msg_processing_core.php';
