369639ed2b4d92e3931cf809018d40cf56690596
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-09-24 19:04:38 +0200

    Ensure that null memory values are purged before store

--- src/lib_memory.php
+++ src/lib_memory.php
@@ -13,6 +13,7 @@ require_once(dirname(__FILE__) . '/lib_database.php');
 
 function memory_load_for_user($telegram_id) {
     $data = db_scalar_query("SELECT `data` FROM `conversation_memories` WHERE `telegram_id` = {$telegram_id}");
+
     if(!$data) {
         return new stdClass();
     }
@@ -21,7 +22,13 @@ function memory_load_for_user($telegram_id) {
 }
 
 function memory_persist($telegram_id, $data) {
+    // Clean-up data, removing null entries
+    $data = (object)array_filter((array)$data, function($val) {
+        return !is_null($val);
+    });
+
     $encoded = json_encode($data);
+
     if(db_perform_action("REPLACE INTO `conversation_memories` (`telegram_id`, `data`, `last_update`) VALUES({$telegram_id}, '" . db_escape($encoded) . "', NOW())") === false) {
         Logger::warning('Failed to store conversation memory', __FILE__);
     }
