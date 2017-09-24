1946741d14db8d788dc305ecad84bf76214c0be1
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-09-24 19:05:01 +0200

    Update game state values

--- src/game.php
+++ src/game.php
@@ -4,7 +4,7 @@
  * ===================
  * UWiClab, University of Urbino
  * ===================
- * Game logic.
+ * Game data.
  */
 
 const STATE_NEW             = 0;  // newly registered, captcha given
@@ -16,7 +16,8 @@ const STATE_GAME_LOCATION   = 30; // [puzzle solved], location assigned, waiting
 const STATE_GAME_SELFIE     = 32; // location reached, qr code scanned, waiting for selfie
 const STATE_GAME_PUZZLE     = 34; // selfie taken, puzzle assigned
 const STATE_GAME_LAST_LOC   = 40; // last location assigned, waiting for qr code
-const STATE_GAME_LAST_PUZ   = 50; // qr code scanned, last puzzle assigned
+const STATE_GAME_LAST_SELF  = 45; // last location reached, asked for selfie
+const STATE_GAME_LAST_PUZ   = 50; // selfie taken, final puzzle assigned
 const STATE_GAME_WON        = 99; // final qrcode scanned, victory
 
 const STATE_ALL             = array(
@@ -29,18 +30,29 @@ const STATE_ALL             = array(
     STATE_GAME_SELFIE,
     STATE_GAME_PUZZLE,
     STATE_GAME_LAST_LOC,
+    STATE_GAME_LAST_SELF,
     STATE_GAME_LAST_PUZ,
     STATE_GAME_WON
 );
 
-const GAME_STATE_NEW                = 0; // newly created
-const GAME_STATE_REG_NAME           = 1; // name registered
-const GAME_STATE_REG_LOCATION       = 2; // location given [ask for address confirmation]
-const GAME_STATE_REG_LOCATION_OK    = 3; // location name confirmed [ask for email]
-const GAME_STATE_REG_EMAIL          = 4; // e-mail given
-const GAME_STATE_REG_CHANNEL        = 5; // channel given
-const GAME_STATE_LOCATION           = 10; // collecting info about a location
-const GAME_STATE_LOCATION_OK        = 11; // accepting a location as completed
-const GAME_STATE_READY              = 20; // all info collected
+const GAME_STATE_NEW                = 0;   // newly created, asked for confirmation
+const GAME_STATE_REG_NAME           = 1;   // confirmed, asked for name
+const GAME_STATE_REG_CHANNEL        = 5;   // name ok, asked for channel
+const GAME_STATE_REG_EMAIL          = 10;  // channel ok, asked for e-mail
+const GAME_STATE_LOCATIONS_FIRST    = 30;  // asked for first location
+const GAME_STATE_LOCATIONS_LAST     = 40;  // asked for last location
+const GAME_STATE_LOCATIONS          = 50;  // asked for location
 const GAME_STATE_ACTIVE             = 128; // ready to accept users, play, etc.
 const GAME_STATE_DEAD               = 255; // game is over
+
+const GAME_STATE_ALL                = array(
+    GAME_STATE_NEW,
+    GAME_STATE_REG_NAME,
+    GAME_STATE_REG_CHANNEL,
+    GAME_STATE_REG_EMAIL,
+    GAME_STATE_LOCATIONS_FIRST,
+    GAME_STATE_LOCATIONS_LAST,
+    GAME_STATE_LOCATIONS,
+    GAME_STATE_ACTIVE,
+    GAME_STATE_DEAD
+);
