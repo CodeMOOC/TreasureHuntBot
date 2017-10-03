da32515fc01a507be6bb3bf64fd0dd405bff42aa
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-10-03 15:08:29 +0200

    Add log details for failed queries

--- src/lib_http.php
+++ src/lib_http.php
@@ -153,6 +153,7 @@ function perform_curl_request($handle) {
         }
     }
 
+    $effective_url = curl_getinfo($handle, CURLINFO_EFFECTIVE_URL);
     curl_close($handle);
 
     if ($http_code >= 500) {
@@ -164,7 +165,7 @@ function perform_curl_request($handle) {
         return false;
     }
     else if ($http_code != 200) {
-        Logger::warning("Request failure with code $http_code ($response)", __FILE__);
+        Logger::warning("Request failure with code $http_code ($response), URL {$effective_url}", __FILE__);
         return false;
     }
     else {
