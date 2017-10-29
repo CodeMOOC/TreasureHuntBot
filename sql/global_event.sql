1cc0df470eae0c6dc53d9c56607e97222fac89ac
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-10-29 09:37:07 +0100

    Add after-event leaderboard generation
    Uses log output to reconstruct victory order of groups.

--- sql/global_event.sql
+++ sql/global_event.sql
@@ -1,6 +1,9 @@
 # Global, per-event leaderboard
 SELECT `groups`.`game_id`, `games`.`name`, `groups`.`group_id`, `groups`.`name`, `groups`.`state`, count(`assigned_locations`.`group_id`) AS `reached`, `groups`.`last_state_change` FROM `games` LEFT OUTER JOIN `groups` ON `games`.`game_id` = `groups`.`game_id` LEFT OUTER JOIN `assigned_locations` ON `assigned_locations`.`game_id` = `games`.`game_id` AND `assigned_locations`.`group_id` = `groups`.`group_id` WHERE `games`.`event_id` = 7 AND `games`.`state` >= 128 GROUP BY `assigned_locations`.`group_id` ORDER BY `reached` DESC, `groups`.`state` DESC, `groups`.`last_state_change` ASC;
 
+# After event leaderboard (via log)
+SELECT `log`.`identity_id`, (SELECT `groups`.`name` FROM `groups` WHERE `groups`.`group_id` = `log`.`identity_id`) AS `team_name`, (SELECT `games`.`game_id`, `games`.`name` FROM `games` WHERE `games`.`game_id` = `log`.`game_id`), `log`.`message`, `log`.`timestamp` FROM `log` WHERE `log`.`game_id` IN (SELECT `games`.`game_id` FROM `games` WHERE `games`.`event_id` = 7) AND `log`.`message` LIKE 'Group has reached the prize%' ORDER BY `log`.`timestamp` ASC;
+
 # Number of groups and participants by game
 SELECT `games`.`name`, count(*), SUM(`groups`.`participants_count`) FROM `games` LEFT OUTER JOIN `groups` ON `games`.`game_id` = `groups`.`game_id` WHERE `games`.`event_id` = 7 AND `games`.`state` >= 128 GROUP BY `games`.`game_id`;
 
