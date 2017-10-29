7626c294928cfeb9c2d7251767c81d535ec1040e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2017-10-29 09:36:37 +0100

    Add individual certificate generation

---
+++ html2pdf/ind-cert-gen.php
@@ -0,0 +1,45 @@
+<?php
+require_once(dirname(__FILE__) . '/vendor/autoload.php');
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+$output_file = $argv[1];     //output PDF path
+$data_participants = $argv[2];
+$data_team_name = $argv[3];
+$data_action = $argv[4];
+$data_game_name = $argv[5];
+$data_identifier = $argv[6];
+
+// instantiate and use the dompdf class
+$input_file = dirname(__FILE__) . '/template-individual-certificate.html';
+$dompdf = new Dompdf();
+$content = file_get_contents($input_file);
+
+//populate html template
+$content = str_replace("%root%", realpath(dirname(__FILE__) . '/../'), $content);
+$content = str_replace("%NUMBER%", $data_participants, $content);
+$content = str_replace("%TEAM_NAME%", $data_team_name, $content);
+$content = str_replace("%ACTION%", $data_action, $content);
+$content = str_replace("%GAME_NAME%", $data_game_name, $content);
+$content = str_replace("%BASE_GEN_FILE%", $data_identifier, $content);
+
+$dompdf->loadHtml($content, 'UTF-8');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+$pdf_gen = $dompdf->output();
+
+if(!file_put_contents($output_file, $pdf_gen)) {
+    echo 'Not OK!';
+    exit(1);
+}
+else {
+    echo 'OK';
+}
+
+exit(0);
---
+++ src/script_individual_certificates.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * CodeMOOC TreasureHuntBot
+ * ===================
+ * UWiClab, University of Urbino
+ * ===================
+ * Generates individual certificates for members of a game.
+ */
+
+require_once(dirname(__FILE__) . '/game.php');
+require_once(dirname(__FILE__) . '/lib.php');
+require_once(dirname(__FILE__) . '/model/context.php');
+
+require_once(dirname(__FILE__) . '/msg_processing_state.php');
+
+if(!isset($argv[1])) {
+    die("Usage: " . basename(__FILE__) . " <GAME ID>\n");
+}
+$game_id = intval($argv[1]);
+
+$groups = db_table_query(sprintf(
+    'SELECT `groups`.`group_id`, `groups`.`name`, `groups`.`state`, `games`.`name` FROM `games` LEFT OUTER JOIN `groups` ON `games`.`game_id` = `groups`.`game_id` WHERE `games`.`game_id` = %d AND `groups`.`state` > %d',
+    $game_id,
+    STATE_REG_READY
+));
+
+printf("Found %d teams in game %d.\n", sizeof($groups), $game_id);
+
+foreach($groups as $group_info) {
+    printf("Processing team '%s' #%d...\n", $group_info[1], $group_info[0]);
+
+    // Generate certificate and montages
+    $reached_locations_count = db_scalar_query(sprintf(
+        'SELECT count(*) FROM `assigned_locations` WHERE `game_id` = %d AND `group_id` = %d AND `reached_on` IS NOT NULL',
+        $game_id,
+        $group_info[0]
+    ));
+    $total_locations_count = $reached_locations_count + 1 + (($group_info[2] > STATE_GAME_LAST_SELF) ? 1 : 0); // start and end
+
+    if($group_info[0] != 291)
+        continue;
+
+    $rootdir = realpath(dirname(__FILE__) . '/..');
+    $identifier = "{$game_id}-{$group_info[0]}";
+
+    printf("Generating montage...\n");
+
+    exec("montage {$rootdir}/data/selfies/{$identifier}-*.jpg -background \"#0000\" -auto-orient -geometry 150x150 +polaroid -tile {$total_locations_count}x1 {$rootdir}/data/certificates/individual/{$identifier}-montage.png");
+
+    printf("Generating certificate...\n");
+
+    exec("php {$rootdir}/html2pdf/ind-cert-gen.php \"{$rootdir}/data/certificates/individual/{$identifier}-certificate.pdf\" 0 \"{$group_info[1]}\" \"completed\" \"{$group_info[3]}\" \"{$identifier}\"");
+}
